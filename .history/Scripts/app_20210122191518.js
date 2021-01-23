3f951f0a46533fbb79c616ff8f0bcaed0a0a7311
Eirk <erikhayward1998@gmail.com>
2021-01-22 19:16:27 -0500

    initial commit

---
+++ .history/Scripts/app_20210122191518.js
@@ -0,0 +1,15 @@
+/* Custom Java Script goes here */
+
+// IIFE - Immediately Invoked Function Expression 
+// AKA - Anonymous Self-Executing Function 
+// Closure - limits scope leak 
+
+(function(){
+
+    function Start(){
+        console.log("App Started...");
+    }
+
+    window.addEventListener("load", Start);
+
+})();
